1eee8fb1755bdf5bdb03cd5158c1dc01957a1de1
Pratikghotaneo <pratikghotaneo@gmail.com>
2024-04-24 15:44:16 +0530

    changes

--- src/components/SkillComponent.jsx
+++ src/components/SkillComponent.jsx
@@ -5,7 +5,7 @@ const SkillComponent = ({ skill, showProgress = false }) => {
     <div className="mt-2">
       <p className="text-white font-serif text-sm">{skill.name}</p>
       {showProgress && (
-        <div class="w-full bg-gray-200 rounded-full h-1.5 my-1 dark:bg-gray-500">
+        <div class="w-full bg-gray-600 rounded-full h-1.5 my-1 dark:bg-gray-300">
           <div
             class={`bg-white h-1.5 rounded-full dark:bg-white ${skill.percentage}`}
           ></div>
